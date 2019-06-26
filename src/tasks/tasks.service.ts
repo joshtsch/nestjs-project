8824619361494598db59a6b03a38488246e0a58c
JoshTsch <joshtsch106@gmail.com>
2019-06-26 00:35:28 -0400

    feat: add additional validation to task modules

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -34,17 +34,23 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description, status } = createTaskDto;
+    const { title, description } = createTaskDto;
 
     const task: Task = {
       id: uuid(),
       title,
       description,
-      status: status ? status : TaskStatus.OPEN,
+      status: TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
@@ -52,7 +58,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
