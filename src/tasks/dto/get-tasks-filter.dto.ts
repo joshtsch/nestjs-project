a8b55779c9c533c482d4d7a99a44b2dc2b9cd01d
JoshTsch <joshtsch106@gmail.com>
2019-06-25 22:19:25 -0400

    feat: add methods for filtering and updating tasks

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,6 @@
 import { TaskStatus } from '../task.model';
 
-export class CreateTaskDto {
-  title: string;
-  description: string;
+export class GetTasksFilterDto {
   status: TaskStatus;
+  search: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,18 +12,39 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter(task => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(
+        task =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+
+    return tasks
+      .filter(task => task.status === status)
+      .filter(task => task.description.includes(search));
+  }
+
   getTaskById(id: string): Task {
     return this.tasks.find(task => task.id === id);
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
+    const { title, description, status } = createTaskDto;
 
     const task: Task = {
       id: uuid(),
       title,
       description,
-      status: TaskStatus.OPEN,
+      status: status ? status : TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
@@ -31,5 +53,11 @@ export class TasksService {
 
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
 }
